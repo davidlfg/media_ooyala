1c1a875e8f101f2d07d0cc1b218eed036e41feb2
davidlfg <davidlfg@gmail.com>
2013-09-18 22:53:15 -0500

    media ooyala version 1

---
+++ themes/media-ooyala-video.tpl.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @file media_ooyala/themes/media-ooyala-video.tpl.php
+ *
+ * Template file for theme('media_ooyala_html').
+ *
+ */
+
+?>
+<div class="<?php print $classes; ?> media-ooyala-<?php print $id; ?>">
+  <?php print $markup; ?>
+</div>
